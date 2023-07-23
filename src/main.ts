cf4b4ba2dd162bca1180ce913321de5b832b4ac6
rayleigh420 <nhatduy0409@gmail.com>
2023-07-23 10:34:54 +0700

    Fix eslint

--- src/main.ts
+++ src/main.ts
@@ -3,7 +3,7 @@ import { AppModule } from './app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  app.enableCors({ credentials: true, origin: 'http://localhost:19000' };
+  app.enableCors({ credentials: true, origin: 'http://localhost:19000' });
   await app.listen(process.env.BACKEND_PORT || 3500);
 }
 bootstrap();
