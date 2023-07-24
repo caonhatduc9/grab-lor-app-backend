69728e637a1e23aae068b1f8dcc9229c28d71fe4
CaoDuc <caonhatduc9@gmail.com>
2023-07-24 21:10:41 +0700

    feat: forgot password aand change password

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,14 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
-
+import { ConfigService } from '@nestjs/config';
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+  const configService = app.get(ConfigService);
+  console.log("Frontend URL== ", configService.get('FRONTEND_URL'));
+  app.enableCors({
+    origin: configService.get('FRONTEND_URL'),
+    credentials: true
+  });
   await app.listen(process.env.BACKEND_PORT || 3500);
 }
 bootstrap();
