477e05bd5c642bf94936b7a14b20cdab95ba0ae0
CaoDuc <caonhatduc9@gmail.com>
2023-09-06 10:27:15 +0700

    update cors

--- src/main.ts
+++ src/main.ts
@@ -6,7 +6,7 @@ async function bootstrap() {
   const configService = app.get(ConfigService);
   console.log('Frontend URL== ', configService.get('FRONTEND_URL'));
   app.enableCors({
-    origin: [configService.get('FRONTEND_URL'), "localhost:5173"],
+    origin: [configService.get('FRONTEND_URL'), "http://localhost:5173"],
     credentials: true,
   });
   await app.listen(process.env.BACKEND_PORT || 3500);
