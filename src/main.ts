6203758f2f61d953b702f0d5958a86647afae903
CaoDuc <caonhatduc9@gmail.com>
2023-09-08 10:27:51 +0700

    update cors

--- src/main.ts
+++ src/main.ts
@@ -6,7 +6,7 @@ async function bootstrap() {
   const configService = app.get(ConfigService);
   console.log('Frontend URL== ', configService.get('FRONTEND_URL'));
   app.enableCors({
-    origin: [configService.get('FRONTEND_URL'), "http://localhost:5173"],
+    origin: [configService.get('FRONTEND_URL'), "http://localhost:5173", "https://ktpm-staff-center-website-client-app.vercel.app"],
     credentials: true,
   });
   await app.listen(process.env.BACKEND_PORT || 3500);
